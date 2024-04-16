a9ff3cc283617c6fe6750a4e8d97f70bf1b1ab2e
jturner1825 <159961957+jturner1825@users.noreply.github.com>
2024-04-16 18:15:00 -0400

    Added updated changing of scenes to each GUI

--- SemesterProject/src/main/LoginPage.java
+++ SemesterProject/src/main/LoginPage.java
@@ -3,6 +3,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -20,99 +21,100 @@ import java.io.FileNotFoundException;
 
 public class LoginPage extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Create top menu buttons
-        Button homeButton = createStyledButton("Home");
-        Button collectionsButton = createStyledButton("Collections");
+	@Override
+	public void start(Stage primaryStage) {
+		
+		// Innitialize ComboBox
+		String[] options = { "Home", "Bookshelf", "Games" };
+		ComboBox<String> pageSelection = new ComboBox<String>();
+		pageSelection.getItems().addAll(options);
+		pageSelection.setValue("Login");
+		pageSelection.setStyle("-fx-font-size: 16px; -fx-text-fill: #00008B;");
 
-        // Lock buttons to the top left and top right respectively
-        HBox leftMenu = new HBox(10, homeButton);
-        HBox rightMenu = new HBox(10, collectionsButton);
-        leftMenu.setAlignment(Pos.TOP_LEFT);
-        rightMenu.setAlignment(Pos.TOP_RIGHT);
+		pageSelection.setOnAction(e -> {
+			String selectedOption = pageSelection.getValue();
+			System.out.println("Selected Option: " + selectedOption);
+			SwitchScene.switchScene(selectedOption, primaryStage);
+		});
 
-        // Set padding for the right menu to create a fixed distance from the top right corner
-        leftMenu.setPadding(new Insets(10, 0, 0, 10));
-        rightMenu.setPadding(new Insets(10, 10, 0, 0));
+		// Create EventHandler for clicking on image
+		Image logo = new Image("https://renaissancepsa.com/wp-content/uploads/2017/08/rams_logo.png");
+		ImageView imageView = null;
+		imageView = new ImageView(logo);
+		imageView.setPickOnBounds(true);
+		imageView.setFitWidth(200); // Set desired width
+		imageView.setFitHeight(100); // Set desired height
+		imageView.setOnMouseClicked(e -> {
+			getHostServices().showDocument("https://renaissancepsa.com/");
+		});
 
-        // Create an ImageView with the specified image file
-        //Image logo = new Image("https://renaissancepsa.com/wp-content/uploads/2017/08/rams_logo.png");
-        //ImageView ivLogo = new ImageView(logo);
-        //ivLogo.setPickOnBounds(true); // Make ImageView contain entire image, not just the geometrical shape
+		// Create centered label for login
+		Label loginLabel = createStyledLabel("Login");
 
-        // Create EventHandler for clicking on image
-        Image logo = new Image("https://renaissancepsa.com/wp-content/uploads/2017/08/rams_logo.png");
-        ImageView imageView = null;
-            imageView = new ImageView(logo);
-            imageView.setPickOnBounds(true);
-            imageView.setFitWidth(200); // Set desired width
-            imageView.setFitHeight(100); // Set desired height
-            imageView.setOnMouseClicked(e -> {
-                getHostServices().showDocument("https://renaissancepsa.com/");
-            });
+		// Create a VBox to stack the image and the label vertically
+		VBox vbox = new VBox(10, imageView, loginLabel);
+		vbox.setAlignment(Pos.TOP_CENTER);
+		
+		// Create HBox
+		HBox hbox = new HBox(10, pageSelection, vbox);
+		hbox.setAlignment(Pos.BOTTOM_CENTER);
 
-        // Create centered label for login
-        Label loginLabel = createStyledLabel("Login");
+		// Create labels and text fields for username and password
+		Label usernameLabel = createStyledLabel("Username:");
+		TextField usernameField = createStyledTextField();
 
-        // Create a VBox to stack the image and the label vertically
-        VBox vbox = new VBox(10, imageView, loginLabel);
-        vbox.setAlignment(Pos.TOP_CENTER);
+		Label passwordLabel = createStyledLabel("Password:");
+		TextField passwordField = createStyledTextField();
 
-        // Create labels and text fields for username and password
-        Label usernameLabel = createStyledLabel("Username:");
-        TextField usernameField = createStyledTextField();
+		// Create login button
+		Button loginButton = createStyledButton("Login");
 
-        Label passwordLabel = createStyledLabel("Password:");
-        TextField passwordField = createStyledTextField();
+		// Create layout
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(10));
 
-        // Create login button
-        Button loginButton = createStyledButton("Login");
+		// Center form
+		GridPane centerForm = new GridPane();
+		centerForm.addRow(0, usernameLabel, usernameField);
+		centerForm.addRow(1, passwordLabel, passwordField);
+		centerForm.setVgap(10);
+		centerForm.setHgap(10);
+		centerForm.setAlignment(Pos.CENTER);
 
-        // Create layout
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10));
+		StackPane topPane = new StackPane();
+		topPane.getChildren().addAll(hbox);
+		root.setTop(topPane);
+		BorderPane.setMargin(topPane, new Insets(0,0,0,-20));
+		root.setCenter(centerForm);
+		root.setBottom(loginButton);
+		BorderPane.setAlignment(loginButton, Pos.BOTTOM_CENTER);
 
-        // Center form
-        GridPane centerForm = new GridPane();
-        centerForm.addRow(0, usernameLabel, usernameField);
-        centerForm.addRow(1, passwordLabel, passwordField);
-        centerForm.add(loginButton, 1, 2);
-        centerForm.setVgap(10);
-        centerForm.setHgap(10);
-        centerForm.setAlignment(Pos.CENTER);
+		// Create scene and set it to the stage
+		Scene scene = new Scene(root, 400, 300);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Login Form");
+		primaryStage.show();
+	}
 
-        StackPane topPane = new StackPane();
-        topPane.getChildren().addAll(leftMenu, rightMenu, vbox);
-        root.setTop(topPane);
-        root.setCenter(centerForm);
+	private Button createStyledButton(String text) {
+		Button button = new Button(text);
+		button.setStyle("-fx-font-size: 14pt; -fx-font-weight: bold;");
+		return button;
+	}
 
-        // Create scene and set it to the stage
-        Scene scene = new Scene(root, 400, 300);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Login Form");
-        primaryStage.show();
-    }
+	private Label createStyledLabel(String text) {
+		Label label = new Label(text);
+		label.setStyle("-fx-font-size: 14pt; -fx-font-weight: bold;");
+		return label;
+	}
 
-    private Button createStyledButton(String text) {
-        Button button = new Button(text);
-        button.setStyle("-fx-font-size: 14pt; -fx-font-weight: bold;");
-        return button;
-    }
+	private TextField createStyledTextField() {
+		TextField textField = new TextField();
+		textField.setStyle("-fx-font-size: 14pt;");
+		return textField;
+	}
 
-    private Label createStyledLabel(String text) {
-        Label label = new Label(text);
-        label.setStyle("-fx-font-size: 14pt; -fx-font-weight: bold;");
-        return label;
-    }
-
-    private TextField createStyledTextField() {
-        TextField textField = new TextField();
-        textField.setStyle("-fx-font-size: 14pt;");
-        return textField;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- SemesterProject/src/main/MoreDetail.java
+++ SemesterProject/src/main/MoreDetail.java
@@ -9,7 +9,6 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class MoreDetail extends Application {
-    // Declare GUI components
     private Label titleLabel;
     private Label authorLabel;
     private Label authorNameLabel;
@@ -17,48 +16,62 @@ public class MoreDetail extends Application {
     private Label pageAmountLabel;
     private Label genreLabel;
     private Label genreOfBookLabel;
-    private Label copiesAmountLabel; // Changed label name
-    private Label copiesAmountValueLabel; // Changed variable name
-    private Button bookshelfButton; // Changed button name
-    private ComboBox<String> dropDownButton; // Drop-down button
-    private StackPane imagePanel; // StackPane to center the image
+    private Label copiesAmountLabel;
+    private Label copiesAmountValueLabel;
+    private Button bookshelfButton;
+    private StackPane imagePanel;
 
-    // Declare variables
     private String bookTitle;
     private String authorName;
     private String pageAmount;
     private String genreOfBook;
-    private String copiesAmount; // Changed variable name
+    private String copiesAmount;
+    
+    public MoreDetail() {
+        // Default values
+        this.bookTitle = "Unknown";
+        this.authorName = "Unknown Author";
+        this.pageAmount = "0";
+        this.genreOfBook = "Unknown";
+        this.copiesAmount = "0";
+    }
+
+    public MoreDetail(String bookTitle) {
+        this.bookTitle = bookTitle;
+        // You can set other book details based on the selected book title here.
+        // For now, I'm setting some example values.
+        this.authorName = "Unknown Author";
+        this.pageAmount = "300";
+        this.genreOfBook = "Fiction";
+        this.copiesAmount = "5";
+    }
 
     @Override
     public void start(Stage primaryStage) {
         // Initialize components
-        titleLabel = new Label("Book Title: ");
+        titleLabel = new Label("Book Title: " + bookTitle);
         authorLabel = new Label("Author: ");
-        authorNameLabel = new Label();
+        authorNameLabel = new Label(authorName);
         pageLabel = new Label("Page #: ");
-        pageAmountLabel = new Label();
+        pageAmountLabel = new Label(pageAmount);
         genreLabel = new Label("Genre: ");
-        genreOfBookLabel = new Label();
-        copiesAmountLabel = new Label("Copies Amount: "); // Changed label name
-        copiesAmountValueLabel = new Label(); // Changed variable name
-        bookshelfButton = new Button("Bookshelf"); // Changed button name
-        String[] options = {"Home", "Bookshelf", "Login", "Games"}; // Changed drop-down options order
-        dropDownButton = new ComboBox<>();
-        dropDownButton.getItems().addAll(options);
-        dropDownButton.setValue(options[0]); // Set initial value
-        dropDownButton.setStyle("-fx-font-size: 24px; -fx-text-fill: #00008B;"); // Set font size and color for drop-down button
-        imagePanel = new StackPane(); // Use StackPane to center the image
+        genreOfBookLabel = new Label(genreOfBook);
+        copiesAmountLabel = new Label("Copies Amount: ");
+        copiesAmountValueLabel = new Label(copiesAmount);
+        bookshelfButton = new Button("Bookshelf");
+        imagePanel = new StackPane();
 
-        // Set default values
-        bookTitle = "Unknown";
-        authorName = "Unknown";
-        pageAmount = "0";
-        genreOfBook = "Unknown";
-        copiesAmount = "0"; // Changed default value
+        // Initialize ComboBox
+        String[] options = { "Home", "Bookshelf", "Games" };
+        ComboBox<String> pageSelection = new ComboBox<String>();
+        pageSelection.getItems().addAll(options);
+        pageSelection.setStyle("-fx-font-size: 24px; -fx-text-fill: #00008B;");
 
-        // Update the title label with the current book title
-        updateTitleLabel();
+        pageSelection.setOnAction(e -> {
+            String selectedOption = pageSelection.getValue();
+            System.out.println("Selected Option: " + selectedOption);
+            SwitchScene.switchScene(selectedOption, primaryStage);
+        });
 
         // Set font size and color for labels and buttons
         String font = "Arial";
@@ -70,9 +83,9 @@ public class MoreDetail extends Application {
         pageAmountLabel.setFont(new javafx.scene.text.Font(font, fontSize));
         genreLabel.setFont(new javafx.scene.text.Font(font, fontSize));
         genreOfBookLabel.setFont(new javafx.scene.text.Font(font, fontSize));
-        copiesAmountLabel.setFont(new javafx.scene.text.Font(font, fontSize)); // Changed label font
-        copiesAmountValueLabel.setFont(new javafx.scene.text.Font(font, fontSize)); // Changed variable font
-        bookshelfButton.setFont(new javafx.scene.text.Font(font, fontSize)); // Changed button font
+        copiesAmountLabel.setFont(new javafx.scene.text.Font(font, fontSize));
+        copiesAmountValueLabel.setFont(new javafx.scene.text.Font(font, fontSize));
+        bookshelfButton.setFont(new javafx.scene.text.Font(font, fontSize));
 
         // Set text color to dark blue for all components
         String darkBlueColor = "-fx-text-fill: #00008B;";
@@ -83,61 +96,50 @@ public class MoreDetail extends Application {
         pageAmountLabel.setStyle(darkBlueColor);
         genreLabel.setStyle(darkBlueColor);
         genreOfBookLabel.setStyle(darkBlueColor);
-        copiesAmountLabel.setStyle(darkBlueColor); // Changed label color
-        copiesAmountValueLabel.setStyle(darkBlueColor); // Changed variable color
-        bookshelfButton.setStyle(darkBlueColor); // Changed button color
+        copiesAmountLabel.setStyle(darkBlueColor);
+        copiesAmountValueLabel.setStyle(darkBlueColor);
+        bookshelfButton.setStyle(darkBlueColor);
 
         // Add components to the top section
         VBox topPanel = new VBox(20);
         topPanel.setAlignment(Pos.CENTER);
-        topPanel.getChildren().addAll(imagePanel); // Changed button position
+        topPanel.getChildren().addAll(imagePanel);
 
-        // Add Drop-down button to the top right
-        HBox rightSection = new HBox(dropDownButton);
-        rightSection.setAlignment(Pos.TOP_RIGHT); // Align the drop-down button to the top
-        rightSection.setPadding(new Insets(10));
-        rightSection.setSpacing(20); // Add some spacing between components
-        
-     // Add Bookshelf button to the top left
-        HBox leftSection = new HBox(bookshelfButton);
+        // Add Bookshelf button to the top left
+        HBox leftSection = new HBox(pageSelection);
         leftSection.setAlignment(Pos.TOP_LEFT);
         leftSection.setPadding(new Insets(10));
 
-        // Set the size of the drop-down button
-        dropDownButton.setPrefWidth(300); // Adjust button width
-        dropDownButton.setPrefHeight(60); // Adjust button height
-        
         // Add sections to the top panel
-        topPanel.getChildren().addAll(leftSection, rightSection);
+        topPanel.getChildren().addAll(leftSection);
 
         // Load and add logo image
         try {
             Image logoImage = new Image("https://renaissancepsa.com/wp-content/uploads/2017/08/rams_logo.png");
             ImageView logoView = new ImageView(logoImage);
             logoView.setPreserveRatio(true);
-            logoView.setFitWidth(200); // Adjust image size as needed
+            logoView.setPickOnBounds(true);
+            logoView.setFitWidth(200);
             imagePanel.getChildren().add(logoView);
-            logoView.setOnMouseClicked(this::openLink); // Set click event handler for the image
+            logoView.setOnMouseClicked(this::openLink);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         // Add components to the center section
-        VBox centerPanel = new VBox(20); // Changed layout to VBox
-        centerPanel.setAlignment(Pos.CENTER); // Set alignment for the whole VBox
+        VBox centerPanel = new VBox(20);
+        centerPanel.setAlignment(Pos.CENTER);
         centerPanel.getChildren().addAll(
-                // Labels aligned to the center
                 titleLabel,
-                // Labels aligned to the center left
                 new HBox(20, authorLabel, authorNameLabel),
                 new HBox(20, pageLabel, pageAmountLabel),
                 new HBox(20, genreLabel, genreOfBookLabel),
-                new HBox(20, copiesAmountLabel, copiesAmountValueLabel) // Changed label name
+                new HBox(20, copiesAmountLabel, copiesAmountValueLabel)
         );
-        
+
         // Add delete button to the bottom right
         Button deleteButton = new Button("Delete");
-        deleteButton.setVisible(false); // Initially hidden
+        deleteButton.setVisible(false);
         HBox bottomRightBox = new HBox();
         bottomRightBox.setAlignment(Pos.BOTTOM_RIGHT);
         bottomRightBox.getChildren().add(deleteButton);
@@ -145,7 +147,6 @@ public class MoreDetail extends Application {
 
         // Set background color to light gray for the center panel
         centerPanel.setStyle("-fx-background-color: #F0F0F0;");
-        rightSection.setStyle("-fx-background-color: #F0F0F0;");
         leftSection.setStyle("-fx-background-color: #F0F0F0;");
         topPanel.setStyle("-fx-background-color: #F0F0F0;");
 
@@ -158,20 +159,10 @@ public class MoreDetail extends Application {
         // Set stage properties
         primaryStage.setScene(scene);
         primaryStage.setTitle("MoreDetail");
-        primaryStage.setMaximized(true); // Maximize the stage
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
-    // Method to update the title label with the current book title
-    private void updateTitleLabel() {
-        titleLabel.setText("Book Title: " + bookTitle);
-        authorNameLabel.setText(authorName);
-        pageAmountLabel.setText(pageAmount);
-        genreOfBookLabel.setText(genreOfBook);
-        copiesAmountValueLabel.setText(copiesAmount); // Changed variable name
-    }
-
-    // Method to handle opening the link
     private void openLink(MouseEvent event) {
         getHostServices().showDocument("https://renaissancepsa.com/");
     }
